f4ce59a72efe6603eddede2d06b1f483c8891096
shahrukh60 <shahrukhahmad226@gmail.com>
2024-10-14 01:35:54 -0700

    my first things
    dffzczxczxczxczx

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+import daz,{b,c}   from './module2.mjs'
+console.log(daz);
+console.log(b);
+console.log(c);
+
---
+++ module2.mjs
@@ -0,0 +1,7 @@
+ const a="Amin";
+ const b="khan";
+ const c = "noor";
+
+ export default a;
+ export{b};
+ export{c};
